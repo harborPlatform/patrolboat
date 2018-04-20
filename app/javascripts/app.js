bddc776ca9e726f2726a0e3ac5a5566722a7d645
yoriba <fistline@naver.com>
2018-04-20 12:39:35 +0900

    test

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -4,63 +4,63 @@
 // import '../../bower_components/tachyons/css/tachyons.min.css'
 // import '../../bower_components/animate.css/animate.min.css'
 import '../stylesheets/app.css'
+// import { default as tfc } from 'truffle-contract'
 
-var Web3 = require('web3');
+var Web3 = require('web3')
 
-window.app = {
-
-}
-
-window.onLoadVeiw = {
-  loadNomalWeb3:function(){
-  console.log('standard web3 init !!!')
-  var web3 = new Web3();
-      // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-      //window.web3 = new Web3(web3.currentProvider);
-      window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-    } else {
-      // window.web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-      // var provider = new Web3.providers.HttpProvider("http://localhost:8545");
-       window.web3 = new Web3(new Web3.providers.HttpProvider("http://etherscan.io:8545"));
+window.pb = {}
+window.pb.provider = {
+  default:function () {
+    this.changeProvider('mainnet')
+  },
+  changeProvider:function (network, ips, port) {
+    if (network === 'undefined' || network === '') { return }
+    window.web3 = new Web3()
+    if (network === 'mainnet') {
+      window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
+    } else if (network === 'custom') {
+      window.web3 = new Web3(new Web3.providers.HttpProvider('http://' + ips + ':' + port));
+    } else if (network === 'ropsten') {
+      window.web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
     }
   }
-  ,checkNetwork:function(){
 
-      web3.version.getNetwork((err, netId) => {
-      switch (netId) {
-        case "1":
-          console.log('This is mainnet');
-          alert('this is mainnet');
-          break
-        case "2":
-          console.log('This is the deprecated Morden test network.');
-          break
-        case "3":
-          console.log('This is the ropsten test network.');
-          break
-        default:
-          console.log('This is an unknown network.');
-        }
-       });
-    }
+}//참고
+//https://ethereum.stackexchange.com/questions/31928/call-a-contract-with-web3js-ethereumjs-tx?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
+window.pb.wallet = {
+  loadDefault:function () {
+    window.pb.wallet.list = window.web3.eth.accounts
+  }
 }
 
-onLoadVeiw.loadNomalWeb3();
+// window.pb.contract = {
+//   loadtfc:function (_json) {
+//     var tfc_obj = tfc(_json)
+//     tfc_obj.setProvider(window.web3.currentProvider)
+//     tfc_obj.deployed().then(function (inst) {
+//       var obj = tfc_obj.at(inst.address);
+//     })
+//   },
+//   testCall:function (obj,fnc,context,txarg) {
+//     obj[fun].apply(concext,txarg)
 
-var acs  = web3.eth.accounts;
+//   }
+// }
 
+window.pb.provider.default()
+window.pb.wallet.loadDefault()
 
-$(document).on('ready', function() {
-    $("#input-b6").fileinput({
-        showUpload: false,
-        dropZoneEnabled: false,
-        maxFileCount: 10,
-        mainClass: "input-group-lg"
-    });
+console.log(window.web3.eth)
 
-    $("#input-b6").onchange(function(){
-         //submit the form here
-         alert('ss')
-    });
-});
+/*
+wallet structure
+  <데이터 영역>
+    지갑주소: 고유식별자
+   금액: Eth 잔액
+   EthRC20:배열 데이터
+  <View영역>
+   별칭:임이의 이름
+   아바타: 별칭 이미지이며, 임이의 이미지
+   아바타성별
+   아바타 번호
+*/
---
+++ app/javascripts/sample.js
@@ -0,0 +1,327 @@
+
+// var ethJSABI = require("ethjs-abi");
+// var BlockchainUtils = require("truffle-blockchain-utils");
+
+// 	detectNetwork: function() {
+//       var self = this;
+//       return new Promise(function(accept, reject) {
+//         // Try to detect the network we have artifacts for.
+//         if (self.network_id) {
+//           // We have a network id and a configuration, let's go with it.
+//           if (self.networks[self.network_id] != null) {
+//             return accept(self.network_id);
+//           }
+//         }
+
+//         self.web3.version.getNetwork(function(err, result) {
+//           if (err) return reject(err);
+
+//           var network_id = result.toString();
+
+//           // If we found the network via a number, let's use that.
+//           if (self.hasNetwork(network_id)) {
+//             self.setNetwork(network_id);
+//             return accept();
+//           }
+
+//           // Otherwise, go through all the networks that are listed as
+//           // blockchain uris and see if they match.
+//           var uris = Object.keys(self._json.networks).filter(function(network) {
+//             return network.indexOf("blockchain://") == 0;
+//           });
+
+//           var matches = uris.map(function(uri) {
+//             return BlockchainUtils.matches.bind(BlockchainUtils, uri, self.web3.currentProvider);
+//           });
+
+//           Utils.parallel(matches, function(err, results) {
+//             if (err) return reject(err);
+
+//             for (var i = 0; i < results.length; i++) {
+//               if (results[i]) {
+//                 self.setNetwork(uris[i]);
+//                 return accept();
+//               }
+//             }
+
+//             // We found nothing. Set the network id to whatever the provider states.
+//             self.setNetwork(network_id);
+
+//             accept();
+//           });
+
+//         });
+//       });
+//     }
+
+//   var Utils = {
+//     is_object: function(val) {
+//       return typeof val == "object" && !Array.isArray(val);
+//     },
+//     is_big_number: function(val) {
+//       if (typeof val != "object") return false;
+
+//       // Instanceof won't work because we have multiple versions of Web3.
+//       try {
+//         new BigNumber(val);
+//         return true;
+//       } catch (e) {
+//         return false;
+//       }
+//     },
+//     decodeLogs: function(C, instance, logs) {
+//       return logs.map(function(log) {
+//         var logABI = C.events[log.topics[0]];
+
+//         if (logABI == null) {
+//           return null;
+//         }
+
+//         // This function has been adapted from web3's SolidityEvent.decode() method,
+//         // and built to work with ethjs-abi.
+
+//         var copy = Utils.merge({}, log);
+
+//         function partialABI(fullABI, indexed) {
+//           var inputs = fullABI.inputs.filter(function (i) {
+//             return i.indexed === indexed;
+//           });
+
+//           var partial = {
+//             inputs: inputs,
+//             name: fullABI.name,
+//             type: fullABI.type,
+//             anonymous: fullABI.anonymous
+//           };
+
+//           return partial;
+//         }
+
+//         var argTopics = logABI.anonymous ? copy.topics : copy.topics.slice(1);
+//         var indexedData = "0x" + argTopics.map(function (topics) { return topics.slice(2); }).join("");
+//         var indexedParams = ethJSABI.decodeEvent(partialABI(logABI, true), indexedData);
+
+//         var notIndexedData = copy.data;
+//         var notIndexedParams = ethJSABI.decodeEvent(partialABI(logABI, false), notIndexedData);
+
+//         copy.event = logABI.name;
+
+//         copy.args = logABI.inputs.reduce(function (acc, current) {
+//           var val = indexedParams[current.name];
+
+//           if (val === undefined) {
+//             val = notIndexedParams[current.name];
+//           }
+
+//           acc[current.name] = val;
+//           return acc;
+//         }, {});
+
+//         Object.keys(copy.args).forEach(function(key) {
+//           var val = copy.args[key];
+
+//           // We have BN. Convert it to BigNumber
+//           if (val.constructor.isBN) {
+//             copy.args[key] = C.web3.toBigNumber("0x" + val.toString(16));
+//           }
+//         });
+
+//         delete copy.data;
+//         delete copy.topics;
+
+//         return copy;
+//       }).filter(function(log) {
+//         return log != null;
+//       });
+//     },
+//     promisifyFunction: function(fn, C) {
+//       var self = this;
+//       return function() {
+//         var instance = this;
+
+//         var args = Array.prototype.slice.call(arguments);
+//         var tx_params = {};
+//         var last_arg = args[args.length - 1];
+
+//         // It's only tx_params if it's an object and not a BigNumber.
+//         if (Utils.is_object(last_arg) && !Utils.is_big_number(last_arg)) {
+//           tx_params = args.pop();
+//         }
+
+//         tx_params = Utils.merge(C.class_defaults, tx_params);
+
+//         return C.detectNetwork().then(function() {
+//           return new Promise(function(accept, reject) {
+//             var callback = function(error, result) {
+//               if (error != null) {
+//                 reject(error);
+//               } else {
+//                 accept(result);
+//               }
+//             };
+//             args.push(tx_params, callback);
+//             fn.apply(instance.contract, args);
+//           });
+//         });
+//       };
+//     },
+//     synchronizeFunction: function(fn, instance, C) {
+//       var self = this;
+//       return function() {
+//         var args = Array.prototype.slice.call(arguments);
+//         var tx_params = {};
+//         var last_arg = args[args.length - 1];
+
+//         // It's only tx_params if it's an object and not a BigNumber.
+//         if (Utils.is_object(last_arg) && !Utils.is_big_number(last_arg)) {
+//           tx_params = args.pop();
+//         }
+
+//         tx_params = Utils.merge(C.class_defaults, tx_params);
+
+//         return C.detectNetwork().then(function() {
+//           return new Promise(function(accept, reject) {
+//             var callback = function(error, tx) {
+//               if (error != null) {
+//                 reject(error);
+//                 return;
+//               }
+
+//               var timeout = C.synchronization_timeout || 240000;
+//               var start = new Date().getTime();
+
+//               var make_attempt = function() {
+//                 C.web3.eth.getTransactionReceipt(tx, function(err, receipt) {
+//                   if (err) return reject(err);
+
+//                   if (receipt != null) {
+//                     return accept({
+//                       tx: tx,
+//                       receipt: receipt,
+//                       logs: Utils.decodeLogs(C, instance, receipt.logs)
+//                     });
+//                   }
+
+//                   if (timeout > 0 && new Date().getTime() - start > timeout) {
+//                     return reject(new Error("Transaction " + tx + " wasn't processed in " + (timeout / 1000) + " seconds!"));
+//                   }
+
+//                   setTimeout(make_attempt, 1000);
+//                 });
+//               };
+
+//               make_attempt();
+//             };
+
+//             args.push(tx_params, callback);
+//             fn.apply(self, args);
+//           });
+//         });
+//       };
+//     },
+//     merge: function() {
+//       var merged = {};
+//       var args = Array.prototype.slice.call(arguments);
+
+//       for (var i = 0; i < args.length; i++) {
+//         var object = args[i];
+//         var keys = Object.keys(object);
+//         for (var j = 0; j < keys.length; j++) {
+//           var key = keys[j];
+//           var value = object[key];
+//           merged[key] = value;
+//         }
+//       }
+
+//       return merged;
+//     },
+//     parallel: function (arr, callback) {
+//       callback = callback || function () {};
+//       if (!arr.length) {
+//         return callback(null, []);
+//       }
+//       var index = 0;
+//       var results = new Array(arr.length);
+//       arr.forEach(function (fn, position) {
+//         fn(function (err, result) {
+//           if (err) {
+//             callback(err);
+//             callback = function () {};
+//           } else {
+//             index++;
+//             results[position] = result;
+//             if (index >= arr.length) {
+//               callback(null, results);
+//             }
+//           }
+//         });
+//       });
+//     },
+//     bootstrap: function(fn) {
+//       // Add our static methods
+//       Object.keys(fn._static_methods).forEach(function(key) {
+//         fn[key] = fn._static_methods[key].bind(fn);
+//       });
+
+//       // Add our properties.
+//       Object.keys(fn._properties).forEach(function(key) {
+//         fn.addProp(key, fn._properties[key]);
+//       });
+
+//       return fn;
+//     }
+//   };
+  
+// function Contract(contract) {
+//     var self = this;
+//     var constructor = this.constructor;
+//     this.abi = constructor.abi;
+
+//     if (typeof contract == "string") {
+//       var address = contract;
+//       var contract_class = constructor.web3.eth.contract(this.abi);
+//       contract = contract_class.at(address);
+//     }
+
+//     this.contract = contract;
+
+//     // Provision our functions.
+//     for (var i = 0; i < this.abi.length; i++) {
+//       var item = this.abi[i];
+//       if (item.type == "function") {
+//         if (item.constant == true) {
+//           this[item.name] = Utils.promisifyFunction(contract[item.name], constructor);
+//         } else {
+//           this[item.name] = Utils.synchronizeFunction(contract[item.name], this, constructor);
+//         }
+
+//         this[item.name].call = Utils.promisifyFunction(contract[item.name].call, constructor);
+//         this[item.name].sendTransaction = Utils.promisifyFunction(contract[item.name].sendTransaction, constructor);
+//         this[item.name].request = contract[item.name].request;
+//         this[item.name].estimateGas = Utils.promisifyFunction(contract[item.name].estimateGas, constructor);
+//       }
+
+//       if (item.type == "event") {
+//         this[item.name] = contract[item.name];
+//       }
+//     }
+
+//     this.sendTransaction = Utils.synchronizeFunction(function(tx_params, callback) {
+//       if (typeof tx_params == "function") {
+//         callback = tx_params;
+//         tx_params = {};
+//       }
+
+//       tx_params.to = self.address;
+
+//       constructor.web3.eth.sendTransaction.apply(constructor.web3.eth, [tx_params, callback]);
+//     }, this, constructor);
+
+//     this.send = function(value) {
+//       return self.sendTransaction({value: value});
+//     };
+
+//     this.allEvents = contract.allEvents;
+//     this.address = contract.address;
+//     this.transactionHash = contract.transactionHash;
+//   };
